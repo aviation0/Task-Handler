0fc057b730bff6c638470cbc24c95dd218555714
Avi Anand <avi123321123321@gmail.com>
2019-05-14 15:25:07 +0530

    Setup express router

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,4 +4,8 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-api', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false
+}).then(() => {
+  console.log("Connection to database has now been established.");
+}).catch((e) => {
+  console.log("Database Error, Please check connection.");
 });
