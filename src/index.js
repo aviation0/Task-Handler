984f7cd655e11f1b7b0918cd00df4ea20a027d32
Avi Anand <avi123321123321@gmail.com>
2019-05-17 11:51:28 +0530

    Add req log middleware

--- src/index.js
+++ src/index.js
@@ -6,9 +6,18 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+  console.log(req.method, req.path);
+  next();
+});
+
+// //Maintenance middleware
+// app.use((req, res, next) => {
+//   res.status(503).send("Server is under maintainance");
+// });
+
 //To parse json from body
 app.use(express.json());
-
 //Routers
 app.use(userRouter);
 app.use(taskRouter);
@@ -16,3 +25,15 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log('Server is up on port: ' + port);
 })
+
+// const jwt = require("jsonwebtoken");
+
+// const myFunction = async () => {
+//   const token = jwt.sign({ _id: 'abc123' }, 'heythere', { expiresIn: '7 days' });
+//   console.log(token);
+
+//   const data = jwt.verify(token, 'heythere');
+//   console.log(data);
+// };
+
+// myFunction();
